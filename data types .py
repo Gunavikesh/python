b4415e15e8e33b8127aab3579e8e86369611a251
Gunavikesh <55891470+Gunavikesh@users.noreply.github.com>
2020-07-17 17:29:12 +0530

    Create data types .py
    data types,operators,casting

---
+++ data types .py
@@ -0,0 +1,135 @@
+# python
+python startup
+                         #2.INTEGERS AND FLOATS
+# DATA TYPES
+
+x = 3   #int
+print(type(x))
+
+y = 3.223333
+print(type(y))
+
+z = "guna"
+print(type(z))
+
+a = 'w'
+print(type(a))
+
+a = True
+print(type(a))
+
+a=[1,2,3]
+print(type(a))
+
+x = 1j
+print(type(x))
+
+x = ["apple", "banana", "cherry"]
+print(type(x))
+
+x = ("apple", "banana", "cherry")
+print(type(x))
+
+x = range(6)
+print(type(x))
+
+x = {"name" : "John", "age" : 36}
+print(type(x))
+
+x = {"apple", "banana", "cherry"}
+print(type(x))
+
+x = frozenset({"apple", "banana", "cherry"})
+print(type(x))
+
+x = b"Hello"		
+print(type(x))
+
+x = bytearray(5)
+print(type(x))
+
+x = memoryview(bytes(5))
+print(type(x))
+
+#ARITHEMATIC OPERATIONS
+
+x = 3
+y = 2
+
+print(x+y) #ADDITION
+print(x-y) #DIFFERENCE
+print(x*y) #MULTIPLICATION
+print(x/y) #DIVISION
+print(x%y) #MODULUS
+print(x**y)#Exponentiation
+print(x//y)#FLOOR DIVISION
+
+#INCREMENTATION
+
+i = 1
+i=i+1
+print(i)
+
+#DECREMENTATION
+
+i = 2
+i=i-1
+print(i)
+
+#ROUNDING A NUMBER
+
+A = 2.9
+b = round(A)
+print(b)
+
+
+A = 2.5
+b = round(A)
+print(b)
+
+print(round(9.99))
+
+
+#ABSOLUTE VALUE
+
+print(abs(-3))
+
+#COMPARISIONS
+
+a = 100
+b = 59
+
+
+print(a == b)#equal
+print(a != b)#not equal
+print(a > b) #greater than
+print(a < b) #less than
+print(a >= b)#Greater than or equal to
+print(a <= b)#less than or equal to
+
+
+#CASTING
+x = int(1)
+y = int(2.8)
+z = int("3")
+print(x)
+print(y)
+print(z)
+
+x = float(1)
+y = float(2.8)
+z = float("3")
+w = float("4.2")
+print(x)
+print(y)
+print(z)
+print(w)
+
+x = str("s1")
+y = str(2)
+z = str(3.0)
+print(x)
+print(y)
+print(z)
+
+ 
